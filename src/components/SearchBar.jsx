724dfa83c19f14c511830aea6131e10236e34f7b
Shohzod <xorozovshohzod@gmail.com>
2022-11-08 12:05:21 +0300

    fixed auto complite

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,7 +7,8 @@ function SearchBar() {
         id="search"
         type="text"
         placeholder="Поиск по названию товара"
-        className="flex-1 outline-none text-base text-darkColor leading-[0px] tracking-[-0.3px] placeholder:text-greyColor pl-5 py-3 bg-none"
+        className="flex-1 outline-none text-base  text-darkColor leading-[0px] tracking-[-0.3px] placeholder:text-greyColor pl-5 py-3 !bg-none"
+        autoComplete="off"
       />
       <button className="bg-greenColor py-[13px] px-[38px] rounded-r-[26px]">
         <label htmlFor="search" className="cursor-pointer">
